9684cf70c160e896a5babbc3c02c751e8e576e28
alexIonescuD <dionescu90@yahoo.com>
2022-11-02 18:16:54 +0200

    update

--- Models/Author.cs
+++ Models/Author.cs
@@ -10,6 +10,16 @@ namespace Ionescu_Alex_Daniel_Lab2.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
         public ICollection<Author>? Authors { get; set; }
+        public ICollection<Book>? Books { get; set; }
     }
 }
---
+++ Models/ViewData/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Ionescu_Alex_Daniel_Lab2.Models.ViewData
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
